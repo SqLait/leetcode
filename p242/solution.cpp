4d72e31e6432685dca0d9a1cd86f5935425c5b59
SqLait <119504721+SqLait@users.noreply.github.com>
2026-03-03 16:53:56 +0100

    solved: p242
    This is a trash solution but I will come back to this one.

---
+++ p242/solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t) {
+        std::unordered_map<unsigned char, short> unmap;
+        for (int i = 0; i < s.size(); i++) {
+            unmap[s[i]] += 1;
+        }
+
+        for (int i = 0; i < t.size(); i++) {
+            if ((unmap[t[i]] - 1) < 0) {
+                return false;
+            } else {
+                unmap[t[i]] -= 1;
+            }
+        }
+
+        for (auto i : unmap) {
+            if (i.second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    s.isAnagram("anagram", "nagaram");
+}
